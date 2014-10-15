7992414b664266632fd5c7f00859bfb5620960dc
mohd kamarshad <mkamarshad@gmail.com>
2014-10-15 13:37:25 +0530

    New Project created and going to add it on gitRepo

---
+++ GitLearning/GitLearning/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitLearning
+//
+//  Created by Mohd Kamarshad on 15/10/14.
+//  Copyright (c) 2014 Mohd Kamarshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitLearning/GitLearning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitLearning
+//
+//  Created by Mohd Kamarshad on 15/10/14.
+//  Copyright (c) 2014 Mohd Kamarshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
